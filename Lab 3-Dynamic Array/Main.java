61ea91d3ed259987403230e932553acd2a77551b
jairvaldovinos <jvaldovinos20052005@gmail.com>
2026-02-14 19:37:33 -0800

    Main.java

--- Lab 3-Dynamic Array/Main.java
+++ Lab 3-Dynamic Array/Main.java
@@ -1,1 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        DynamicArray<Integer> arr = new DynamicArray<>();
+
+        for (int i = 0; i < 12; i++) {
+            arr.add(i);
+        }
+
+        System.out.println("Size: " + arr.size());
+        System.out.println("Item at index 5: " + arr.get(5));
+
+        System.out.println("Removed: " + arr.remove(2));
+
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.println(arr.get(i));
+        }
+    }
+}
 
